e49a829ce182d1051b08a3a7777527912479fc28
Alexander Faßbender <brotenko@gmail.com>
2021-01-10 23:41:28 +0100

    Prepared implementation of SignalR.

--- ServerLogic/PlayerAudienceClient/Scripts/site.js
+++ ServerLogic/PlayerAudienceClient/Scripts/site.js
@@ -1,3 +1,9 @@
-﻿window.onload = function () {
-    /* FALL THROUGH */()
+﻿function validateKey(e)
+{
+    document.write(JSON.stringify($('form').serializeArray()));
+}
+
+window.onload = function () {
+    /* FALL THROUGH */
+    document.getElementById("sessionsubmit").addEventListener("submit", validateKey, false);
 }
