4b4b7f033d4a8622d2d58a8d1a6b3d947a54706a
Alexander Faßbender <brotenko@gmail.com>
2021-01-12 21:15:44 +0100

    Fianlized the cookie/ToS consent script and prepared the HTML/CSS for the dynamic generation of voting prompts.

--- ServerLogic/PlayerAudienceClient/Scripts/site.js
+++ ServerLogic/PlayerAudienceClient/Scripts/site.js
@@ -1,7 +1,42 @@
 ﻿function validateKey(e)
 {
-    document.write(JSON.stringify($('form').serializeArray()));
+    document.write(JSON.stringify($('#sessionsubmit').serializeArray()));
 }
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+$(document).ready(function () {
+    var myCookie = getCookie("CookieConsent");
+    if (myCookie == "") {
+        $('#landing-page').css("visibility", "unset");
+    }
+});
+
+$(function () {
+    $("#consent-text").load("ConsentText.html");
+});
+
+$(function () { //shorthand document.ready function
+    $('#consent-form').on('submit', function (e) {
+        e.preventDefault();  //prevent form from submitting
+        document.cookie = "CookieConsent=true";
+        $('#landing-page').css("visibility", "hidden");
+    });
+});
 
 window.onload = function () {
     /* FALL THROUGH */
