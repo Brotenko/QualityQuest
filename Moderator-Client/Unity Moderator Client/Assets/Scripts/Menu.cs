5fbd93a72dca20b8bb822b320a70f51d3e0980d0
Manuel Werner <manuel.werner@uni-ulm.de>
2021-01-26 19:30:16 +0100

    created first functional menu

---
+++ Moderator-Client/Unity Moderator Client/Assets/Scripts/Menu.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject audioMenu;
+    public GameObject optionsMenu;
+    public GameObject languageMenu;
+    public GameObject displayMenu;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ShowMainMenu()
+    {
+        HideAllMenu();
+        mainMenu.SetActive(true);
+    }
+
+    public void ShowAudioMenu()
+    {
+        HideAllMenu();
+        audioMenu.SetActive(true);
+    }
+
+    public void ShowOptionsMenu()
+    {
+        HideAllMenu();
+        optionsMenu.SetActive(true);
+    }
+
+    public void ShowLanguageMenu()
+    {
+        HideAllMenu();
+        languageMenu.SetActive(true);
+    }
+
+    public void ShowDisplayMenu()
+    {
+        HideAllMenu();
+        displayMenu.SetActive(true);
+    }
+
+    public void HideAllMenu()
+    {
+        audioMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(false);
+        languageMenu.SetActive(false);
+        displayMenu.SetActive(false);
+    }
+
+}
