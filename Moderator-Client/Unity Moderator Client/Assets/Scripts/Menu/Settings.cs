d8b39f2918df046ed7815df2620463b23b7d7723
mandalax <65391687+mandalax@users.noreply.github.com>
2021-03-13 17:54:07 +0100

    Added refresh rate in the resolution options and adjusted scroll speed.

--- Moderator-Client/Unity Moderator Client/Assets/Scripts/Menu/Settings.cs
+++ Moderator-Client/Unity Moderator Client/Assets/Scripts/Menu/Settings.cs
@@ -55,7 +55,7 @@ public class Settings : MonoBehaviour
         int currentResolutionIndex = 0;
 
         for (int i = 0; i < resolutions.Length; i++) {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = resolutions[i].width + " x " + resolutions[i].height + " - " + resolutions[i].refreshRate + " Hz";
             options.Add(option);
 
             if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
