be09b72d5e61dbf5963df64efc684db137a05d5b
mandalax <65391687+mandalax@users.noreply.github.com>
2021-03-16 14:19:56 +0100

    Added the StoryEvent class.

---
+++ Moderator-Client/QualityQuest/Assets/Scripts/Story/StoryEvent.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryEvent
+{
+    private int eventId;
+    private string description;
+    private StoryEvent parent;
+    private HashSet<StoryEvent> children;
+
+    /// <summary>
+    /// Constructor of the StoryEvent class.
+    /// </summary>
+    /// <param name="eventId">The Id of the StoryEvent.</param>
+    /// <param name="description">The description of the StoryEvent.</param>
+    /// <param name="parent">The parent StoryEvent of the StoryEvent.</param>
+    /// <param name="children">The children StoryEvents of the StoryEvent.</param>
+    public StoryEvent(int eventId, string description, StoryEvent parent, HashSet<StoryEvent> children)
+    {
+        this.eventId = eventId;
+        this.description = description;
+        this.parent = parent;
+        this.children = children;
+    }
+
+    /// <summary>
+    /// Getter for the eventId of a StoryEvent.
+    /// </summary>
+    /// <returns>The eventId of the StoryEvent.</returns>
+    public int getEventId()
+    {
+        return eventId;
+    }
+
+    /// <summary>
+    /// Getter for the destription of a StoryEvent.
+    /// </summary>
+    /// <returns>The description of the StoryEvent</returns>
+    public string getDescription()
+    {
+        return description;
+    }
+
+    /// <summary>
+    /// Getter for the parent of a StoryEvent.
+    /// </summary>
+    /// <returns>The parent of the StoryEvent</returns>
+    public StroyEvent getParent()
+    {
+        return parent;
+    }
+
+    /// <summary>
+    /// Getter for the children of a StoryEvent.
+    /// </summary>
+    /// <returns>The children of the StoryEvent.</returns>
+    public HashSet<StoryEvent> getChildren()
+    {
+        return children;
+    }
+
+    /// <summary>
+    /// Method to add a child to a StoryEvent.
+    /// </summary>
+    /// <param name="child">The child that is added.</param>
+    public void addChild(StoryEvent child)
+    {
+        children.Add(child);
+    }
+ 
+}
