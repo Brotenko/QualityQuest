647530b629caf5f653da05f03d341eb8b279b4be
mandalax <65391687+mandalax@users.noreply.github.com>
2021-03-27 19:05:54 +0100

    Set password for the TestClient.

--- Moderator-Client/QualityQuest/Assets/Scripts/WebSocket/Client.cs
+++ Moderator-Client/QualityQuest/Assets/Scripts/WebSocket/Client.cs
@@ -175,7 +175,7 @@ public class Client : MonoBehaviour
 
     public void RequestOpenSessionMessage()
     {
-        MessageContainer.Messages.RequestOpenSessionMessage test = new MessageContainer.Messages.RequestOpenSessionMessage(testGuid, "");
+        MessageContainer.Messages.RequestOpenSessionMessage test = new MessageContainer.Messages.RequestOpenSessionMessage(testGuid, "Passw0rd");
 
         SendMessage(test);
     }
