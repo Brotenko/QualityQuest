2023f4cc3259b27fc882f1ea20f23ac6425b9db7
mandalax <65391687+mandalax@users.noreply.github.com>
2021-04-20 08:26:11 +0200

    Updated activeScreenManager with the new panels for the online mode.

--- Moderator-Client/QualityQuest/Assets/Scripts/Game/ActiveScreenManager.cs
+++ Moderator-Client/QualityQuest/Assets/Scripts/Game/ActiveScreenManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ActiveScreenManager : MonoBehaviour
 {
@@ -16,13 +18,20 @@ public class ActiveScreenManager : MonoBehaviour
     public GameObject connect;
     public GameObject qrCode;
     public GameObject statistics;
+    public GameObject errorScreen;
     public QRCode qrCodeGenerator;
+
+    public TMP_Text gameMenuSwitchModeButton;
     public TMP_Text audienceCount;
     public TMP_Text websiteUrl;
     public TMP_Text sessionKey;
+    public TMP_Text pauseUrl;
+    public TMP_Text pauseKey;
+    public TMP_Text errorMessage;
+    public Button pauseButton;
 
     private GameObject activeMenu;
-    private bool paused = false;
+    public static bool paused;
 
     private void Awake()
     {
@@ -30,12 +39,12 @@ public class ActiveScreenManager : MonoBehaviour
         HideAllMenus();
         if (Menu.gameIsOnline)
         {
-            
             activeMenu = connect;
             connect.SetActive(true);
         }
         else
         {
+            pauseButton.gameObject.SetActive(false);
             activeMenu = characterSelection;
             characterSelection.SetActive(true);
         }
@@ -43,6 +52,8 @@ public class ActiveScreenManager : MonoBehaviour
 
     public void HideAllMenus()
     {
+        gameMenu.SetActive(false);
+        errorScreen.SetActive(false);
         connect.SetActive(false);
         qrCode.SetActive(false);
         characterSelection.SetActive(false);
@@ -50,6 +61,17 @@ public class ActiveScreenManager : MonoBehaviour
         storyflow.SetActive(false);
         result.SetActive(false);
         statistics.SetActive(false);
+    }
+
+    public void ShowConnection()
+    {
+        activeMenu = connect;
+
+        if (!gameMenu.activeSelf && !paused)
+        {
+            HideAllMenus();
+            connect.SetActive(true);
+        }
     }
 
     public void ShowStatistics()
@@ -60,6 +82,18 @@ public class ActiveScreenManager : MonoBehaviour
         {
             HideAllMenus();
             statistics.SetActive(true);
+        }
+    }
+
+    public void ShowErrorScreen(string errorMessage)
+    {
+        activeMenu = errorScreen;
+
+        if (!gameMenu.activeSelf)
+        {
+            HideAllMenus();
+            errorScreen.SetActive(true);
+            this.errorMessage.text = errorMessage;
         }
     }
 
@@ -84,7 +118,7 @@ public class ActiveScreenManager : MonoBehaviour
             qrCode.SetActive(true);
             websiteUrl.text = url;
             sessionKey.text = key;
-            qrCodeGenerator.GenerateQRCode(url);
+            qrCodeGenerator.GenerateQRCode(url,1);
             audienceCount.text = "Verbundene Spieler (0)";
             Debug.Log("3");
         }
@@ -127,7 +161,7 @@ public class ActiveScreenManager : MonoBehaviour
         }
     }
 
-    public void ShowPauseMenu()
+    public void ShowPauseMenu(string url, string sessionKey)
     {
 
         if (!paused)
@@ -138,6 +172,9 @@ public class ActiveScreenManager : MonoBehaviour
             {
                 HideAllMenus();
                 pauseScreen.SetActive(true);
+                pauseKey.text = sessionKey;
+                pauseUrl.text = url;
+                qrCodeGenerator.GenerateQRCode("www.twitch.tv/maxolus", 2);
             }
 
         }
@@ -155,6 +192,15 @@ public class ActiveScreenManager : MonoBehaviour
 
     public void ShowGameMenu()
     {
+        if (Menu.gameIsOnline)
+        {
+            gameMenuSwitchModeButton.text = "Offline Mode";
+        }
+        else
+        {
+            gameMenuSwitchModeButton.text = "Online Mode";
+        }
+
         if (!gameMenu.activeSelf)
         {
             HideAllMenus();
@@ -177,8 +223,17 @@ public class ActiveScreenManager : MonoBehaviour
             }
 
         }
+    }
 
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
 
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
     }
 
 
