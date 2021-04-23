9fd1e714bcafbf7cde85f819db666f8741b5b591
Manuel Werner <manuel.werner@uni-ulm.de>
2021-04-23 20:07:36 +0200

    added comments to characterselection script

--- Moderator-Client/QualityQuest/Assets/Scripts/Game/CharacterSelection.cs
+++ Moderator-Client/QualityQuest/Assets/Scripts/Game/CharacterSelection.cs
@@ -5,25 +5,33 @@ using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour
 {
+    // scripts which update the displayed characters in the character selection screen
     public DisplayCharacter displayNoruso;
     public DisplayCharacter displayKirogh;
     public DisplayCharacter displayTurgal;
     public DisplayCharacter displayLumati;
 
+    // buttons which are used by the moderator to select a character while in offline mode
     public Button selectOfflineLumati;
     public Button selectOfflineTurgal;
     public Button selectOfflineKirogh;
     public Button selectOfflineNoruso;
 
+    // images of the characters
     public Sprite norusoSprite;
     public Sprite lumatiSprite;
     public Sprite turgalSprite;
     public Sprite kiroghSprite;
 
+    // playable characters
     public Character noruso, lumati, turgal, kirogh;
 
+    /// <summary>
+    ///     Awake is called when the script is loaded
+    /// </summary>
     private void Awake()
     {
+        // initialising all playable characters with associated skills name and image
         noruso = new Character(new Skills(3, 1, 2, 1), "Noruso", norusoSprite);
         lumati = new Character(new Skills(1, 3, 0, 4), "Lumati", lumatiSprite);
         turgal = new Character(new Skills(2, 2, 2, 2), "Turgal", turgalSprite);
@@ -31,6 +39,9 @@ public class CharacterSelection : MonoBehaviour
         SetCharacters();
     }
 
+    /// <summary>
+    /// update characterselection screen with created characters
+    /// </summary>
     public void SetCharacters()
     {
         displayNoruso.UpdateCharacter(noruso);
@@ -39,6 +50,14 @@ public class CharacterSelection : MonoBehaviour
         displayLumati.UpdateCharacter(lumati);
     }
 
+    /// <summary>
+    /// Sets the character for the storygraph.
+    /// Displays the statusbar.
+    /// Updates the skills and the character image in the statusbar.
+    /// </summary>
+    /// <param name="character"></param> character which has been selected for the game by the audience or by the moderator
+    /// <param name="story"></param> script which contains the story graph
+    /// <param name="statusBar"></param> script which updates skills, skillchanges and the character image in the satusbar
     public void InitializeCharacter(Character character, GameStory story, DisplayStatusbar statusBar)
     {
         story.playThrough.Character = character ;
@@ -47,6 +66,9 @@ public class CharacterSelection : MonoBehaviour
         statusBar.SetImage(character.Sprite);
     }
 
+    /// <summary>
+    /// enables the buttons for selecting a character in offline mode
+    /// </summary>
     public void ActivateOfflineCharacterPickButtons()
     {
         selectOfflineKirogh.gameObject.SetActive(true);
