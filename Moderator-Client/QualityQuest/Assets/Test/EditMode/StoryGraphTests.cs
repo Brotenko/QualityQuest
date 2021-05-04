a127475a09c256802b2e42819869e27d0508ea63
Manuel Werner <manuel.werner@uni-ulm.de>
2021-05-04 13:31:53 +0200

    fixed story graph tests

--- Moderator-Client/QualityQuest/Assets/Test/EditMode/StoryGraphTests.cs
+++ Moderator-Client/QualityQuest/Assets/Test/EditMode/StoryGraphTests.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
 using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
-using Random = System.Random;
+
 
 public class StoryGraphTests
 {
+
     private StoryEvent newCurrentEvent;
 
     private StoryGraph story;
@@ -32,130 +29,155 @@ public class StoryGraphTests
     private StoryEvent randomSixOptionOne;
     private StoryEvent randomSixOptionTwo;
 
+    private StoryEvent randomDefaultOptionOne;
+    private StoryEvent randomDefaultOptionTwo;
+
     [SetUp]
     public void TestSetup()
     {
+
         story = new StoryGraph();
-        newCurrentEvent = new StoryEvent(new Guid(), "This is a Description", new HashSet<StoryEvent>(), StoryEventType.StoryFlow);
+
+        newCurrentEvent = new StoryEvent(new Guid(), "NewCurrentEvent", new HashSet<StoryEvent>(), StoryEventType.StoryFlow);
+
         characterOne = new Character(new Skills(0, 0, 0, 0), "Alex", null);
         characterTwo = new Character(new Skills(10, 10, 10, 10), "Elias", null);
 
-        randomRoot = new StoryEvent(Guid.NewGuid(), "Root", new HashSet<StoryEvent>(), StoryEventType.StoryFlow);
+        randomRoot = new StoryEvent(new Guid(), "Root", new HashSet<StoryEvent>(), StoryEventType.StoryFlow);
 
-        randomOneOptionOne = new StoryEvent(Guid.NewGuid(), "DecisionOneFalse", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, false,RandomType.RandomDecisionOne);
-        randomOneOptionTwo = new StoryEvent(Guid.NewGuid(), "DecisionOneTrue", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, true, RandomType.RandomDecisionOne);
+        randomOneOptionOne = new StoryEvent(new Guid(), "DecisionOneFalse", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, false,RandomType.RandomDecisionOne);
+        randomOneOptionTwo = new StoryEvent(new Guid(), "DecisionOneTrue", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, true, RandomType.RandomDecisionOne);
 
-        randomTwoOptionOne = new StoryEvent(new Guid(), "This is a description.", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, false, RandomType.RandomDecisionTwo);
-        randomTwoOptionTwo = new StoryEvent(new Guid(), "This is a description.", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, true, RandomType.RandomDecisionTwo);
+        randomTwoOptionOne = new StoryEvent(new Guid(), "DecisionTwoFalse", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, false, RandomType.RandomDecisionTwo);
+        randomTwoOptionTwo = new StoryEvent(new Guid(), "DecisionTwoTrue", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, true, RandomType.RandomDecisionTwo);
 
-        randomThreeOptionOne = new StoryEvent(new Guid(), "This is a description.", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, false, RandomType.RandomDecisionThree);
-        randomThreeOptionTwo = new StoryEvent(new Guid(), "This is a description.", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, true, RandomType.RandomDecisionThree);
+        randomThreeOptionOne = new StoryEvent(new Guid(), "DecisionThreeFalse", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, false, RandomType.RandomDecisionThree);
+        randomThreeOptionTwo = new StoryEvent(new Guid(), "DecisionThreeTrue", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, true, RandomType.RandomDecisionThree);
 
-        randomSixOptionOne = new StoryEvent(new Guid(), "This is a description.", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, false, RandomType.RandomDecisionSix);
-        randomSixOptionTwo = new StoryEvent(new Guid(), "This is a description.", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, true, RandomType.RandomDecisionSix);
+        randomSixOptionOne = new StoryEvent(new Guid(), "DecisionSixFalse", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, false, RandomType.RandomDecisionSix);
+        randomSixOptionTwo = new StoryEvent(new Guid(), "DecisionSixTrue", new HashSet<StoryEvent>(), StoryEventType.StoryFlow, true, RandomType.RandomDecisionSix);
 
+        randomDefaultOptionOne = new StoryEvent(new Guid(), "DefaultOne",new HashSet<StoryEvent>(),StoryEventType.StoryFlow);
+        randomDefaultOptionTwo = new StoryEvent(new Guid(), "DefaultTwo",new HashSet<StoryEvent>(),StoryEventType.StoryFlow);
 
     }
 
     [Test]
     public void StoryGraphTest()
     {
+
         Assert.AreEqual(story.Root,story.CurrentEvent);
         Assert.AreEqual(null,story.Character);
+
     }
 
     [Test]
     public void SetCurrentEventTest()
     {
+
         Assert.AreNotEqual(newCurrentEvent, story.CurrentEvent);
         story.SetCurrentEvent(newCurrentEvent);
         Assert.AreEqual(newCurrentEvent,story.CurrentEvent);
+
     }
 
     [Test]
+
     public void GetRandomOptionTest_False_RandomDecisionOne()
     {
+
         randomRoot.AddChild(randomOneOptionOne);
         randomRoot.AddChild(randomOneOptionTwo);
         randomStory = new StoryGraph(characterOne, randomRoot, randomRoot);
-        randomStory.GetRandomOption();
-        //randomStory.SetCurrentEvent(randomOneOptionOne);
-        //Assert.AreEqual(2,randomRoot.Children.Count);
-        Assert.AreEqual(randomOneOptionOne, randomStory.CurrentEvent);
-        //Assert.AreEqual(randomOneOptionOne.Description,randomStory.CurrentEvent.Description);
+        Assert.AreEqual(randomOneOptionOne, randomStory.GetRandomOption());
 
     }
 
     [Test]
+
     public void GetRandomOptionTest_True_RandomDecisionOne()
     {
+
         randomRoot.AddChild(randomOneOptionOne);
         randomRoot.AddChild(randomOneOptionTwo);
         randomStory = new StoryGraph(characterTwo, randomRoot, randomRoot);
-        randomStory.GetRandomOption();
-        Assert.AreEqual(randomOneOptionTwo, randomStory.CurrentEvent);
+        Assert.AreEqual(randomOneOptionTwo, randomStory.GetRandomOption());
     }
 
     [Test]
     public void GetRandomOptionTest_False_RandomDecisionTwo()
     {
+
         randomRoot.AddChild(randomTwoOptionOne);
         randomRoot.AddChild(randomTwoOptionTwo);
         randomStory = new StoryGraph(characterOne, randomRoot, randomRoot);
-        randomStory.GetRandomOption();
-        Assert.AreEqual(randomTwoOptionOne, randomStory.CurrentEvent);
+        Assert.AreEqual(randomTwoOptionOne, randomStory.GetRandomOption());
 
     }
+
     [Test]
     public void GetRandomOptionTest_True_RandomDecisionTwo()
     {
+
         randomRoot.AddChild(randomTwoOptionOne);
         randomRoot.AddChild(randomTwoOptionTwo);
         randomStory = new StoryGraph(characterTwo, randomRoot, randomRoot);
-        randomStory.GetRandomOption();
-        Assert.AreEqual(randomTwoOptionTwo, randomStory.CurrentEvent);
+        Assert.AreEqual(randomTwoOptionTwo, randomStory.GetRandomOption());
 
     }
 
     [Test]
     public void GetRandomOptionTest_False_RandomDecisionThree()
     {
-        randomRoot.AddChild(randomTwoOptionOne);
-        randomRoot.AddChild(randomTwoOptionTwo);
+
+        randomRoot.AddChild(randomThreeOptionOne);
+        randomRoot.AddChild(randomThreeOptionTwo);
         randomStory = new StoryGraph(characterOne, randomRoot, randomRoot);
-        randomStory.GetRandomOption();
-        Assert.AreEqual(randomThreeOptionOne, randomStory.CurrentEvent);
+        Assert.AreEqual(randomThreeOptionOne, randomStory.GetRandomOption());
 
     }
+
     [Test]
     public void GetRandomOptionTest_True_RandomDecisionThree()
     {
-        randomRoot.AddChild(randomTwoOptionOne);
-        randomRoot.AddChild(randomTwoOptionTwo);
+
+        randomRoot.AddChild(randomThreeOptionOne);
+        randomRoot.AddChild(randomThreeOptionTwo);
         randomStory = new StoryGraph(characterTwo, randomRoot, randomRoot);
-        randomStory.GetRandomOption();
-        Assert.AreEqual(randomThreeOptionTwo, randomStory.CurrentEvent);
+        Assert.AreEqual(randomThreeOptionTwo, randomStory.GetRandomOption());
 
     }
 
     [Test]
     public void GetRandomOptionTest_False_RandomDecisionSix()
     {
-        randomRoot.AddChild(randomThreeOptionOne);
-        randomRoot.AddChild(randomThreeOptionTwo);
+
+        randomRoot.AddChild(randomSixOptionOne);
+        randomRoot.AddChild(randomSixOptionTwo);
         randomStory = new StoryGraph(characterOne, randomRoot, randomRoot);
-        randomStory.GetRandomOption();
-        Assert.AreEqual(randomSixOptionOne, randomStory.CurrentEvent);
+        Assert.AreEqual(randomSixOptionOne, randomStory.GetRandomOption());
 
     }
+
     [Test]
     public void GetRandomOptionTest_True_RandomDecisionSix()
     {
+
         randomRoot.AddChild(randomSixOptionOne);
         randomRoot.AddChild(randomSixOptionTwo);
         randomStory = new StoryGraph(characterTwo, randomRoot, randomRoot);
-        randomStory.GetRandomOption();
-        Assert.AreEqual(randomSixOptionTwo, randomStory.CurrentEvent);
+        Assert.AreEqual(randomSixOptionTwo, randomStory.GetRandomOption());
+
+    }
+
+    [Test]
+    public void GetRandomOptionTest_Default()
+    {
+
+        randomRoot.AddChild(randomDefaultOptionOne);
+        randomRoot.AddChild(randomDefaultOptionTwo);
+        randomStory = new StoryGraph(characterOne, randomRoot, randomRoot);
+        Assert.AreEqual(randomDefaultOptionOne, randomStory.GetRandomOption());
 
     }
 
